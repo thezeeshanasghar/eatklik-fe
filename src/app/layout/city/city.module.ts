cc0ec00925c2b02fc49c655121bc0796c1f40a2d
thezeeshanasghar <thezeeshanasghar@gmail.com>
2019-04-23 17:05:49 +0500

    remove add page as a routing and some header changes

--- src/app/layout/city/city.module.ts
+++ src/app/layout/city/city.module.ts
@@ -5,9 +5,10 @@ import { CityRoutingModule } from './city-routing.module';
 import { CityComponent } from './city.component';
 import { PageHeaderModule } from 'src/app/shared';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   imports: [CommonModule, CityRoutingModule, PageHeaderModule, ReactiveFormsModule],
-  declarations: [CityComponent]
+  declarations: [CityComponent, AddComponent]
 })
 export class CityModule {}
