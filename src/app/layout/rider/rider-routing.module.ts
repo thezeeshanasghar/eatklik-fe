230da67e42b85577494ae620129e520012f032dd
thezeeshanasghar <thezeeshanasghar@gmail.com>
2019-04-25 10:29:45 +0500

    generate Add page for riderAdd

--- src/app/layout/rider/rider-routing.module.ts
+++ src/app/layout/rider/rider-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RiderComponent } from './rider.component';
+import { AddComponent } from './add/add.component';
 
 const routes: Routes = [
   {
     path: '',
     component: RiderComponent
-  }
+  },
+  { path: 'add', component: AddComponent }
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- src/app/layout/rider/rider.module.ts
+++ src/app/layout/rider/rider.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { RiderRoutingModule } from './rider-routing.module';
 import { RiderComponent } from './rider.component';
 import { PageHeaderModule } from 'src/app/shared';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   imports: [CommonModule, RiderRoutingModule, PageHeaderModule],
-  declarations: [RiderComponent]
+  declarations: [RiderComponent, AddComponent]
 })
 export class RiderModule {}
