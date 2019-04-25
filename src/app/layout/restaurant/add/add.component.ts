7f26500c08d5f645568cdfd91c6a141ce5e865e0
Zeeshan Asghar <thezeeshanasghar@gmail.com>
2019-04-26 00:19:04 +0500

    90 percent complete restaurant add page

--- src/app/layout/restaurant/add/add.component.ts
+++ src/app/layout/restaurant/add/add.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Restaurant } from 'src/app/_model/restaurant';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+import { RestaurantService } from 'src/app/shared/services/restaurant.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +12,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
+  restaurant = new Restaurant();
+  constructor(private http: HttpClient, private restaurantService: RestaurantService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSubmit(form) {
+    this.restaurantService.addRestaurant(this.restaurant).subscribe(
+      res => {
+        this.router.navigate(['/restaurant']);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
+  public uploadLogo = files => {
+    if (files.length === 0) {
+      return;
+    }
+    const fileToUpload = <File>files[0];
+    const formData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+
+    this.http.post(environment.BASE_URL + 'upload', formData).subscribe(response => {
+      this.restaurant.LogoImagePath = response['dbPath'];
+    });
+  }
+
+  public uploadCover = files => {
+    if (files.length === 0) {
+      return;
+    }
+    const fileToUpload = <File>files[0];
+    const formData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+
+    this.http.post(environment.BASE_URL + 'upload', formData).subscribe(response => {
+      this.restaurant.CoverImagePath = response['dbPath'];
+    });
+  }
 }
--- src/app/shared/services/cuisine.service.ts
+++ src/app/shared/services/cuisine.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-import { Cuisine } from 'src/app/_model/Cuisine';
+import { Cuisine } from 'src/app/_model/cuisine';
 
 @Injectable({
   providedIn: 'root'
