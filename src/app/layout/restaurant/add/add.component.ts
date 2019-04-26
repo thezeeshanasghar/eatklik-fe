076136db8798ccb19e58410edb17ba3096d7157a
thezeeshanasghar <thezeeshanasghar@gmail.com>
2019-04-26 12:20:06 +0500

    add validation on restaurant add page

--- src/app/layout/restaurant/add/add.component.ts
+++ src/app/layout/restaurant/add/add.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 import { Restaurant } from 'src/app/_model/restaurant';
 import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators';
 import { RestaurantService } from 'src/app/shared/services/restaurant.service';
 import { Router } from '@angular/router';
 
@@ -12,8 +11,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent implements OnInit {
+
+  public progressLogo: number;
+  public progressCover: number;
+  resourceURL: string;
   restaurant = new Restaurant();
-  constructor(private http: HttpClient, private restaurantService: RestaurantService, private router: Router) {}
+  constructor(private http: HttpClient, private restaurantService: RestaurantService, private router: Router) {
+    this.resourceURL = environment.RESOURCES_URL;
+  }
 
   ngOnInit() {}
 
@@ -36,8 +41,12 @@ export class AddComponent implements OnInit {
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
 
-    this.http.post(environment.BASE_URL + 'upload', formData).subscribe(response => {
-      this.restaurant.LogoImagePath = response['dbPath'];
+    this.http.post(environment.BASE_URL + 'upload', formData, { reportProgress: true, observe: 'events' }).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progressLogo = Math.round((100 * event.loaded) / event.total);
+      } else if (event.type === HttpEventType.Response) {
+        this.restaurant.LogoImagePath = event.body['dbPath'];
+      }
     });
   }
 
@@ -49,8 +58,12 @@ export class AddComponent implements OnInit {
     const formData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
 
-    this.http.post(environment.BASE_URL + 'upload', formData).subscribe(response => {
-      this.restaurant.CoverImagePath = response['dbPath'];
+    this.http.post(environment.BASE_URL + 'upload', formData, { reportProgress: true, observe: 'events' }).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.progressCover = Math.round((100 * event.loaded) / event.total);
+      } else if (event.type === HttpEventType.Response) {
+        this.restaurant.CoverImagePath = event.body['dbPath'];
+      }
     });
   }
 }
