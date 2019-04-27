1cdbe91aef056111c277a2f1782c11943a83113a
thezeeshanasghar <thezeeshanasghar@gmail.com>
2019-04-27 12:03:39 +0500

    change backend urls to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
+  BASE_URL: 'https://13.127.49.104:5001/api/',
+  RESOURCES_URL: 'https://13.127.49.104:5001/'
 
-  BASE_URL: 'http://13.127.49.104:5000/api/',
-  RESOURCES_URL:'http://13.127.49.104:5000/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  BASE_URL: 'http://13.127.49.104:5000/api/',
-  RESOURCES_URL: 'http://13.127.49.104:5000/'
+  BASE_URL: 'https://13.127.49.104:5001/api/',
+  RESOURCES_URL: 'https://13.127.49.104:5001/'
 };
 
 /*
