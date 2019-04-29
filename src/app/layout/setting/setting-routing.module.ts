ae437951afb867b908536b0cb69f8a13aa106ad3
thezeeshanasghar <thezeeshanasghar@gmail.com>
2019-04-29 19:58:06 +0500

    complete Setting--> list, add, delete

--- src/app/layout/setting/setting-routing.module.ts
+++ src/app/layout/setting/setting-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SettingComponent } from './setting.component';
+import { AddComponent } from './add/add.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SettingComponent
-  }
+  },
+  { path: 'add', component: AddComponent}
 ];
 
 @NgModule({
