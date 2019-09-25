4933b22180df7d85c41bdccc59151c55fb61c874
numankhatak <numanalip@gmail.com>
2019-09-25 11:33:55 +0500

    i can added to customer edit pg

--- src/app/layout/customer/customer.module.ts
+++ src/app/layout/customer/customer.module.ts
@@ -7,9 +7,10 @@ import { PageHeaderModule } from 'src/app/shared';
 import { AddComponent } from './add/add.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EditComponent } from './/edit/edit.component';
 
 @NgModule({
-  declarations: [CustomerComponent, AddComponent],
-  imports: [CommonModule, CustomerRoutingModule, PageHeaderModule, FormsModule,NgbModule]
+  declarations: [CustomerComponent, AddComponent, EditComponent],
+  imports: [CommonModule, CustomerRoutingModule, PageHeaderModule, FormsModule, NgbModule]
 })
 export class CustomerModule {}
