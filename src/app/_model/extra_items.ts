d8f6bd5ee13a49e4bc887e41ab933209f566e5fb
theirfanch <chirfan521@gmail.com>
2019-10-15 22:09:27 -0700

    extraitem size  Added

--- src/app/_model/extra_items.ts
+++ src/app/_model/extra_items.ts
@@ -1,6 +1,7 @@
 export class ExtraItem {
   public Id: number;
   public Name: string;
+  public Size : string ;
   public Price: number;
   public ImagePath: string;
   public RestaurantId: number;
