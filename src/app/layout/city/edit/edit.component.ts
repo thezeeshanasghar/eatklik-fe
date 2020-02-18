213358a9a64494e893fdc2360cbf6fef6cbb2325
numankhatak <numanalip@gmail.com>
2020-02-18 11:23:16 +0500

    add the password & enable disable in edit.html(rider)

--- src/app/layout/city/edit/edit.component.ts
+++ src/app/layout/city/edit/edit.component.ts
@@ -14,6 +14,8 @@ import { City } from 'src/app/_model/city';
 export class EditComponent implements OnInit {
   public progress: number;
   public message: string;
+  public message1: string;
+  public uploadProgress: number;
   @Output() public onUploadFinished = new EventEmitter();
 
   cities: City;
@@ -41,21 +43,45 @@ export class EditComponent implements OnInit {
         this.progress = Math.round((100 * event.loaded) / event.total);
       } else if (event.type === HttpEventType.Response) {
         this.message = 'Upload success.';
-        let dbPath: any = event.body;
+        const dbPath: any = event.body;
          this.onUploadFinished.emit(dbPath);
         //  this.model.ImagePath = environment.RESOURCES_URL + dbPath.dbPath;
           this.model.ImagePath = dbPath.dbPath;
 
-        
+
       }
     });
-  };
+  }
+
+
+  public uploadCover = files => {
+    if (files.length === 0) {
+      return;
+    }
+    const fileToUpload = <File>files[0];
+    const formData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+
+    this.http.post(environment.BASE_URL + 'upload', formData, { reportProgress: true, observe: 'events' }).subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress) {
+        this.uploadProgress = Math.round((100 * event.loaded) / event.total);
+      } else if (event.type === HttpEventType.Response) {
+        this.message1 = 'Upload success.';
+        const dbPath: any = event.body;
+         this.onUploadFinished.emit(dbPath);
+        //  this.model.ImagePath = environment.RESOURCES_URL + dbPath.dbPath;
+          this.model.CoverImagePath = dbPath.dbPath;
+
+      }
+    });
+  }
+
 
   getCityByid() {
     this.cityService.getCity(this.id).subscribe(
       res => {
         this.model = res as City;
-        
+
       },
       err => {
         console.log(err);
